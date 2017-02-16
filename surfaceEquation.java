2eccc5916e0b10c97fd2fc2b2d4d5412735e24e9
Andrii Tykhonov <tihonav@gmail.com>
2017-02-16 13:31:28 +0100

    some removes

--- surfaceEquation.java
+++
@@ -1,9 +0,0 @@
-abstract interface surfaceEquation
-{
-  public abstract float Eq(int paramInt1, int paramInt2);
-
-  public abstract int[] getLineQant();
-
-  public abstract double[] getRoomSize();
-}
-
